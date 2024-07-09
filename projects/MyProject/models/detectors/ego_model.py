eeb32c551cfc1557b8ce5a7825f9e7a84501f66b
wangyichen <wangyichen@xiaomi.com>
2024-07-09 18:00:49 +0900

    fix bug

--- projects/MyProject/models/detectors/ego_model.py
+++ projects/MyProject/models/detectors/ego_model.py
@@ -613,13 +613,12 @@ class EgoModel(MVXTwoStageDetector):
                     unified_feat_map = torch.cat((ego_features[0],warp_infra_feat),0)
                     query_key_map = self.query_key_net(unified_feat_map)
                     query_key_map_ego = query_key_map[0:batch_size * 1]
-                    query_key_map_infra = query_key_map[batch_size * 1:batch_size * 2]
                     query = torch.unsqueeze(self.query_net(query_key_map_ego), 1)
                     keys = self.key_net(query_key_map)
                     key_ego = torch.unsqueeze(keys[0:batch_size * 1], 1)
                     key_infra = torch.unsqueeze(keys[batch_size * 1:batch_size * 2], 1)
                     keys = torch.cat((key_ego, key_infra), 1)
-                    vals = torch.cat((ego_features[0], warp_infra_feat), 1)
+                    vals = torch.cat((ego_features[0].unsqueeze(1), warp_infra_feat.unsqueeze(1)), 1)
                     ego_fusion_result = self.attention_net(query, keys, vals)
                 else:
                     if self.train_mode == 'pred_corr':
